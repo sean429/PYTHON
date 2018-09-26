83be394067865bce5ae61262f1fffe97e9ba9a13
sean429 <43199295+sean429@users.noreply.github.com>
2018-09-26 18:57:38 +0800

    Add files via upload
    Make a average of your korean, english, math scores!

---
+++ 3주차연습하기첫번째.py
@@ -0,0 +1,50 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+kor=input("당신의 국어 성적은?")
+
+
+# In[ ]:
+
+
+kor=float(kor)
+
+
+# In[ ]:
+
+
+eng=input("당신의 영어 성적은?")
+
+
+# In[ ]:
+
+
+eng=float(eng)
+
+
+# In[ ]:
+
+
+math=input("당신의 수학 성적은?")
+
+
+# In[ ]:
+
+
+math=float(math)
+
+
+# In[ ]:
+
+
+avg=(kor+eng+math)/3
+
+
+# In[ ]:
+
+
+print("3개 과목의 평균은",avg,"입니다.")
+
